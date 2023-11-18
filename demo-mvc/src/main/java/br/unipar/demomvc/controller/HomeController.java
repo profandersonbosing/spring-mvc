013a1b7e60f4eb00c8f1da945a8c7d7ed25144f6
profandersonbosing <110434492+profandersonbosing@users.noreply.github.com>
2023-11-18 10:05:38 -0300

    Add files via upload

--- demo-mvc/src/main/java/br/unipar/demomvc/controller/HomeController.java
+++ demo-mvc/src/main/java/br/unipar/demomvc/controller/HomeController.java
@@ -1,18 +1,29 @@
 package br.unipar.demomvc.controller;
 
+import br.unipar.demomvc.domain.Disciplina;
+import br.unipar.demomvc.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+
     @GetMapping
-    public String retornaHome() {
-        return "index";
+    public ModelAndView retornaHome() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("totalProf", 0);
+        modelAndView.addObject("totalDisciplinas", disciplinaRepository.findAllByOrderByIdAsc().size());
+
+        return modelAndView;
     }
 
 }
